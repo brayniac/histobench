98ed271effd8edf0b8ef1af7f00400c4294e860f
Brian Martin <brayniac@gmail.com>
2017-05-25 09:25:54 -0700

    better names

--- benches/bench_decaying.rs
+++ benches/bench_decaying.rs
@@ -5,7 +5,7 @@ extern crate histobench;
 use histobench::*;
 
 #[bench]
-fn parse_increment(b: &mut test::Bencher) {
+fn decaying_increment(b: &mut test::Bencher) {
 	let mut histogram = Histogram::new();
 	b.iter(|| histogram.update(1));
 }
--- benches/bench_histogram.rs
+++ benches/bench_histogram.rs
@@ -5,7 +5,7 @@ extern crate histogram;
 use histogram::*;
 
 #[bench]
-fn parse_increment(b: &mut test::Bencher) {
+fn brayniac_increment(b: &mut test::Bencher) {
 	let mut histogram = Histogram::new();
 	b.iter(|| histogram.increment(1));
 }
