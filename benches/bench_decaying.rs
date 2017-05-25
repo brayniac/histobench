6342b8325edb5d8922a493186d188746458701e8
Brian Martin <brayniac@gmail.com>
2017-05-25 09:48:35 -0700

    add stddev() and p90 benches

--- benches/bench_decaying.rs
+++ benches/bench_decaying.rs
@@ -9,3 +9,21 @@ fn decaying_increment(b: &mut test::Bencher) {
 	let mut histogram = Histogram::new();
 	b.iter(|| histogram.update(1));
 }
+
+#[bench]
+fn decaying_stddev(b: &mut test::Bencher) {
+	let mut histogram = Histogram::new();
+	for i in 1..1000 {
+		histogram.update(i);
+	}
+	b.iter(|| assert!(histogram.snapshot().stddev() > 0.0));
+}
+
+#[bench]
+fn decaying_p90(b: &mut test::Bencher) {
+	let mut histogram = Histogram::new();
+	for i in 1..1000 {
+		histogram.update(i);
+	}
+	b.iter(|| assert!(histogram.snapshot().value(0.90) > 0));
+}
--- benches/bench_histogram.rs
+++ benches/bench_histogram.rs
@@ -9,3 +9,21 @@ fn brayniac_increment(b: &mut test::Bencher) {
 	let mut histogram = Histogram::new();
 	b.iter(|| histogram.increment(1));
 }
+
+#[bench]
+fn brayniac_stddev(b: &mut test::Bencher) {
+	let mut histogram = Histogram::new();
+	for i in 1..1000 {
+		let _ = histogram.increment(i);
+	}
+	b.iter(|| assert!(histogram.stddev().is_some()));
+}
+
+#[bench]
+fn brayniac_p90(b: &mut test::Bencher) {
+	let mut histogram = Histogram::new();
+	for i in 1..1000 {
+		let _ = histogram.increment(i);
+	}
+	b.iter(|| assert!(histogram.percentile(90.0).is_ok()));
+}
